56dfbe71b73d4e133e56898b5b009def6094f9db
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-02-20 10:33:30 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //Your JavaScript code will go here!
 window.onload = function() {
-
+    var queue = [];
+    document.getElementById("create-meme").addEventListener("click", function () {
+        var videoInput = document.getElementById("video-url-input").value;
+        queue.push(videoInput);
+    });
 }
