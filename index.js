52a0b2b0af39f45f03b540e70b3db2b9b9502235
Okuribi <chuadrian@outlook.com>
2021-02-20 14:50:47 -0800

    Starting on database stuff.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //Your JavaScript code will go here!
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+
+
+
 window.onload = function() {
     var queue = [];
     document.getElementById("confirm-btn").addEventListener("click", function () {
