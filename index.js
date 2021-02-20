79be0c45208d9e9a67cca2afe19f6c073e9b2601
Okuribi <chuadrian@outlook.com>
2021-02-20 11:40:23 -0800

    Queue!

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ window.onload = function() {
         var videoInput = document.getElementById("video-url-input").value;
         queue.push(videoInput);
         console.log(queue);
+
+        ul = document.getElementById("video-queue");
+        let li = document.createElement('li');
+        li.innerHTML = videoInput;
+        ul.append(li);
     });
 }
