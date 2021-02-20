42ad3063d8ef2212fddd48fc899260aac6822e60
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-02-20 10:37:51 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //Your JavaScript code will go here!
 window.onload = function() {
-
+    var queue = [];
+    document.getElementById("confirm-btn").addEventListener("click", function () {
+        var videoInput = document.getElementById("video-url-input").value;
+        queue.push(videoInput);
+    });
 }
